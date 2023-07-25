eef63567b9ae11c4739623f41b79d68029ab11fc
valentinpate <valentinp261@gmail.com>
2023-07-25 16:56:19 -0300

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,20 @@ function App() {
         <img src={sp} alt="" />
         <h1>api demo</h1>
       </div>
+      <div className="Info">
+        <p>South Park API demo made with spapi, Materialize CSS framework and React Vite. Click on 🛈 to get more information about your selected character.</p>
+      </div>
       <div className="Buttons">
-        <button class="waves-effect waves-light btn-large" onClick={()=>setPagina(pagina-1)} disabled={pagina==1 ? "disabled" : ""}>prev</button>
-        <button class="waves-effect waves-light btn-large" onClick={()=>setPagina(pagina+1)} disabled={pagina==20 ? "disabled" : ""}>next</button>
+        <button className="waves-effect waves-light btn-large" onClick={()=>setPagina(1)} disabled={pagina==1 ? "disabled" : ""}>first</button>
+        <button className="waves-effect waves-light btn-large" onClick={()=>setPagina(pagina-1)} disabled={pagina==1 ? "disabled" : ""}>prev</button>
+        <button className="waves-effect waves-light btn-large" onClick={()=>setPagina(pagina+1)} disabled={pagina==20 ? "disabled" : ""}>next</button>
+        <button className="waves-effect waves-light btn-large" onClick={()=>setPagina(20)} disabled={pagina==20 ? "disabled" : ""}>last</button>
+      </div>
+      <div className="Buttons-Responsive">
+        <button className="waves-effect waves-light btn" onClick={()=>setPagina(1)} disabled={pagina==1 ? "disabled" : ""}>first</button>
+        <button className="waves-effect waves-light btn" onClick={()=>setPagina(pagina-1)} disabled={pagina==1 ? "disabled" : ""}>prev</button>
+        <button className="waves-effect waves-light btn" onClick={()=>setPagina(pagina+1)} disabled={pagina==20 ? "disabled" : ""}>next</button>
+        <button className="waves-effect waves-light btn" onClick={()=>setPagina(20)} disabled={pagina==20 ? "disabled" : ""}>last</button>
       </div>
       <div className="container">
         <div className="Search">
@@ -35,16 +46,16 @@ function App() {
         </div>
         <div className="row">
           <div className="col s12">
-            {personajesFiltrados.map(personaje=>(<div class="card" key={personaje.id}>
+            {personajesFiltrados.map(personaje=>(<div className="card" key={personaje.id}>
               <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">{personaje.name}<i class="material-icons right">open</i></span>
+                <span className="card-title activator grey-text text-darken-4">{personaje.name}<i className="material-icons right">🛈</i></span>
                 <div className="character-url">
                   <p><b>URL:</b></p>
                   <a href={personaje.url}>{personaje.url}</a>
                 </div>
               </div>
               <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">{personaje.name}<i class="material-icons right">close</i></span>
+                <span className="card-title grey-text text-darken-4">{personaje.name}<i className="material-icons right">X</i></span>
                 <div className="card-reveal-content">
                   <p><b>AGE:</b> {personaje.age===null ? "???" : `${personaje.age}`}</p>
                   <p><b>SEX:</b> {personaje.sex}</p>
